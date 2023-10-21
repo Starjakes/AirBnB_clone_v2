baa07a434f31a553b9107d6b43934314e97a7a6c
Starjakes <amehjacobe@gmail.com>
2023-10-21 05:42:46 -0700

    8 states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,33 @@
-<!DOCTYPE html>
-<HTML lang="en">
-    <HEAD>
-        <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-        <H1>States</H1>
-        <UL>
-        {% for state in states.values()|sort(attribute="name") %}
-            <LI>{{ state.id }}: <B>{{ state.name }}</B>
-                <UL>
-                {% for city in state.cities|sort(attribute="name") %}
-                    <LI>{{ city.id }}: <B>{{ city.name }}</B></LI>
-                {% endfor %}
-                </UL>
-            </LI>
-        {% endfor %}
-        </UL>
-    </BODY>
-</HTML>
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+"""
+
+from flask import Flask
+from models import storage
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states_route():
+    """
+    Cities by states: display a HTML page: (inside the tag BODY)
+    Returns:
+        html: template that lists all states sort by name A->Z
+    """
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """
+    After each request remove the current SQLAlchemy Session:
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
