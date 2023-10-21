33166efe63ab63253e5235b2c21381d544890cc5
Starjakes <amehjacobe@gmail.com>
2023-10-21 05:21:53 -0700

    7 states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:                    display "Hello HBNB!"
+            /hbnb:                display "HBNB"
+            /c/<text>:            display "C" + text (replace "_" with " ")
+            /python/<text>:       display "Python" + text (default="is cool")
+            /number/<n>:          display "n is a number" only if int
+            /number_template/<n>: display HTML page only if n is int
+            /number_odd_or_even/<n>: display HTML page; display odd/even info
+            /states_list:         display HTML and state info from storage;
 """
 from models import storage
-from flask import Flask
-from flask import render_template
-
+from models import *
+from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+app.url_map.strict_slashes = False
